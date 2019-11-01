567c4f6a9de81bc20a5dc19dddf34e10eb04821c
Oleg Borschev <bosoleg89@gmail.com>
2019-11-01 17:39:10 +0200

    Updates

---
+++ precache-manifest.9470ef0043c15f1cc578e39d866f681a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a78f2f9069d549dc5d12c8b2a317e9f",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "1823958ef483db929da8",
+    "url": "/goit-react-hw-02-reader/static/css/main.75063d45.chunk.css"
+  },
+  {
+    "revision": "bb575eefffd2fa9d9c13",
+    "url": "/goit-react-hw-02-reader/static/js/2.61f7131d.chunk.js"
+  },
+  {
+    "revision": "1823958ef483db929da8",
+    "url": "/goit-react-hw-02-reader/static/js/main.05c8943c.chunk.js"
+  },
+  {
+    "revision": "46b5d60be30d4a7b06da",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.7bf316d8.js"
+  }
+]);
